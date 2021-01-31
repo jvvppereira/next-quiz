dcf0f3ff5962ad65e3f3334177567de24fd3eba3
jvvppereira <joao.paes@philips.com>
2021-01-31 18:11:41 -0300

    added db.json and changed base styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,49 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import db from "../db.json";
+
+const theme = db.theme;
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
   }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.contrastText};
+    opacity: 1; /* Firefox */
+  }
+  :-ms-input-placeholder { /* Internet Explorer 10-11 */
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  ::-ms-input-placeholder { /* Microsoft Edge */
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+`;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
